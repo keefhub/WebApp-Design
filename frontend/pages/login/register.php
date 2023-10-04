1521d99780c907cbc2c82eef4b149c4da4e460cb
keef <user@email.com>
2023-10-05 00:44:00 +0800

    php-side auth ok

--- frontend/pages/login/register.php
+++ frontend/pages/login/register.php
@@ -1,1 +1,17 @@
-<?php var_dump($_POST) ?>
+<?php
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $username = $_POST["username"];
+    $password = $_POST["password"];
+
+    // Example: Check if username and password are valid
+    if ($username === "user" && $password === "password") {
+        // Successful authentication, redirect to the home page
+        header("Location:../homepage.html");
+        exit(); // Make sure to exit to prevent further execution
+    } else {
+        // Invalid username or password, display an error message or redirect to a login error page
+        echo "Invalid username or password. Please try again.";
+    }
+}
+?>
